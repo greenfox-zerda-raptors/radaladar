2d4a65dc855bd94e8e041b1f09ccfe412b0bbdf3
radaladar <bbiczok@gmail.com>
2016-11-02 17:02:54 +0100

    things and stuff

--- week-03/day-01/src/Workshop21.java
+++ week-03/day-01/src/Workshop21.java
@@ -5,6 +5,7 @@
 public class Workshop21{
     public static void main(String... args){
         String proverb = "If it ain't broke, don't fix it.";
+
         System.out.println(proverb.substring(proverb.indexOf("don't"), proverb.indexOf(".")));
     }
 }
