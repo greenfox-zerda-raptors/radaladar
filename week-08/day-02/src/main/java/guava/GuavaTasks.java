e8376dac77143072287ede9f74f154323623b3f8
radaladar <bbiczok@gmail.com>
2016-12-13 15:16:00 +0100

    guava tasks with cars finished, test runs perfectly

--- week-08/day-02/src/main/java/guava/GuavaTasks.java
+++ week-08/day-02/src/main/java/guava/GuavaTasks.java
@@ -1,7 +1,12 @@
 package guava;
 
-import java.util.Collection;
-import java.util.List;
+import com.google.common.base.Joiner;
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+
+import java.util.*;
 
 /**
  * Created by kicsen on 2016. 11. 30..
@@ -23,44 +28,74 @@ public class GuavaTasks {
         Car car5 = new Car(4, "Volvo", 12900, "V40");
 
         // TODO: Initialize cars list with car variables above. You should use Lists.newArrayList() utils of Guava.
-
+        cars = Lists.newArrayList(car1, car2, car3, car4, car5);
         // END of your implementation part
         this.carTitles = generateCarTitles();
     }
 
     String printCar(Car car) {
         // TODO: Print cars in human readable format without toString implementation. "
-        // Hint: MoreObjects.toStringHelper
-        return null;
+        return MoreObjects.toStringHelper(car)
+                .add("id", car.getId())
+                .add("name", car.getName())
+                .add("price", car.getPrice())
+                .add("model", car.getModel())
+                .toString();
     }
 
     Collection<String> generateCarTitles() {
         // TODO: Create String collection from title of cars. Title format: name (model) e.g.: Volvo (XC90)
         // Hint: You should use Lists.newArrayList() utils of Guava.
         // Advanced hint: You may try Guava Function/Predicate and Collections2.transform too.
-        return null;
+        String str;
+        carTitles = new ArrayList<>();
+        for (Car car : cars) {
+            str = String.format("%s (%s)", car.getName(), car.getModel());
+            carTitles.add(str);
+        }
+        return carTitles;
     }
 
     String concatCarTitles(Collection<String> carTitles) {
         // TODO: Build string from carTitles like: "Audi (A4), Mercedes (), Skoda (null), Volvo (XC90), Volvo (V40)"
         // Hint: You should use Guava Joiner.
-        return null;
+        Joiner joiner = Joiner.on(", ");
+        return joiner.join(carTitles);
     }
 
     Collection<String> generateCarTitlesWithoutEmpty(Collection<Car> cars) {
         // TODO: You should upgrade generateCarTitles functionality here to skip model if it is empty or null.
         // "Audi (A4), Mercedes, Skoda, Volvo (XC90), Volvo (V40)"
         // Hint: You should use Guava Strings.isNullOrEmpty()
-        return null;
+        String str;
+        carTitles = new ArrayList<>();
+        for (Car car : cars) {
+            if (Strings.isNullOrEmpty(car.getModel())) {
+                str = String.format("%s", car.getName());
+            } else {
+            str = String.format("%s (%s)", car.getName(), car.getModel());
+            }
+            carTitles.add(str);
+        }
+        return carTitles;
     }
 
     Collection<Car> findCar(final String name, final Integer priceLimit) {
         // TODO: Check name is not null or empty and throw IllegalArgumentException if it is.
         // TODO: Check priceLimit is not null and throw NullPointerException if it is.
         // TODO: Check priceLimit is greater than 0 and throw IllegalArgumentException if it is not.
+        Preconditions.checkNotNull(name != null);
+        Preconditions.checkNotNull(priceLimit != null);
+        Preconditions.checkArgument(priceLimit > 0);
         // Hint: Use Guava Preconditions utilities.
         // TODO: Retrieve all cars which name contains name parameter and price is less than priceLimit.
-        return null;
+        List<Car> carsList = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getName().equals(name) && car.getPrice() < priceLimit) {
+                carsList.add(car);
+            }
+        }
+        return carsList;
     }
 
     private void print(String line) {
