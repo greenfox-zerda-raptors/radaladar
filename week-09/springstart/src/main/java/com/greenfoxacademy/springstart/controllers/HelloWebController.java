f48e49161a10eb56dc4090cea8dfa81992e10e03
radaladar <bbiczok@gmail.com>
2016-12-19 16:04:27 +0100

    maybe this works

--- week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -15,10 +15,22 @@ public class HelloWebController {
 
     private final AtomicLong counter = new AtomicLong();
 
-    private String[] hellos = {"Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
-            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
-            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
-            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
+    private String[][] hellos = {{"Mirëdita", "red", "14"},
+            {"Ahalan", "blue","12"},
+            {"Parev", "green","40"},
+            {"Zdravei", "brown","22"},
+            {"Nei Ho", "orange","38"},
+            {"Dobrý den", "yellow","10"},
+            {"Ahoj", "pink","16"},
+            {"Goddag", "red","12"},
+            {"Goede dag, Hallo","navy","24"},
+            {"Hello","silver","12"},
+            {"Saluton","black","23"},
+            {"Hei","magenta","6"},
+            {"Bonjour","olive","20"}};
+//            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+//            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+//            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"};
 
     @RequestMapping("/web/greeting")
     public String greeting(Model model, @RequestParam String name) {
